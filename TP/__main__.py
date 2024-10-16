f6991505b6ae4bac118737bb7cf35f3d20e63fd5
imran-maia <hossainimran.maia@gmail.com>
2024-10-16 23:39:46 +0200

    add sketch in kmers and main file

--- TP/__main__.py
+++ TP/__main__.py
@@ -1,39 +1,75 @@
 from TP.loading import load_directory
-from TP.kmers import stream_kmers, kmer2str
+from TP.kmers import stream_kmers, kmer2str, filter_smallest
 
-def jaccard(fileA, fileB, k):
-    
-    # Concatenate the sequences from the list into a single string for each sample
-    seqA = "".join(fileA)
-    seqB = "".join(fileB)
 
-    # Convert the generator into a set to evaluate all the k-mers
-    setA = set(stream_kmers(seqA, k))
-    setB = set(stream_kmers(seqB, k))
 
-    # Calculate the intersection and union
-    intersection = len(setA & setB)
-    union = len(setA | setB)
+def jaccard_from_sorted_lists(lstA, lstB):
+    idxA = 0
+    idxB = 0
 
-    if union == 0:
-        j = 0
-    else:
-        j = intersection / union
+    intersection = 0
+    union = 0
 
-    return j
+    while idxA < len(lstA) and idxB < len(lstB):
+        union += 1
+        if lstA[idxA] == lstB[idxB]:
+            intersection += 1
+            idxA += 1
+            idxB += 1
+        elif lstA[idxA] < lstB[idxB]:
+            idxA += 1
+        else:
+            idxB += 1
+
+    union += len(lstA) - idxA
+    union += len(lstB) - idxB
+
+    return intersection / union
+
+
 
 if __name__ == "__main__":
     print("Computation of Jaccard similarity between files")
 
     # Load all the files in a dictionary
+    print("Loading files")
     files = load_directory("data")
     k = 21
-    
-    print("Computing Jaccard similarity for all pairs of samples")
-    filenames = list(files.keys())
+    s = 10000
 
+    filenames = list(files.keys())
+    
+    # Create all the kmer lists (can be expensive in memory)
+    print("Computing all kmer vectors")
+    kmer_lists = {}
+    for filename in filenames:
+        kmer_lists[filename] = []
+        # Enumerate all the sequences from a fasta
+        for seq in files[filename]:
+            kmer_lists[filename].extend(stream_kmers(seq, k))
+        # Sort the kmer lists to speed up the comparison
+        kmer_lists[filename].sort()
+
+    print("Computing Jaccard similarity for all pairs of samples")
     for i in range(len(files)):
-        for j in range(i + 1, len(files)):
-            
-            j_sim = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(f"{filenames[i]} vs {filenames[j]}: {j_sim}")
+        for j in range(i+1, len(files)):
+            jaccard = jaccard_from_sorted_lists(kmer_lists[filenames[i]], kmer_lists[filenames[j]])
+            print(filenames[i], filenames[j], jaccard)
+    
+    
+    print('--------------------------------')
+    print("Computing sketches for each file")
+    sketches = {}
+    for filename in filenames:
+        combined_seq = "".join(files[filename])  
+        
+        sketch = filter_smallest(combined_seq, k, s)  
+        sketches[filename] = sketch
+        sketches[filename].sort()
+
+
+    print("Computing Jaccard similarity for all pairs of files (using sketches)")
+    for i in range(len(filenames)):
+        for j in range(i + 1, len(filenames)):
+            jaccard = jaccard_from_sorted_lists(sketches[filenames[i]], sketches[filenames[j]])
+            print(filenames[i], filenames[j], jaccard)
